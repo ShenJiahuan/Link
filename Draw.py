d44157e1cc16c89aa865650d508aca129603af12
沈嘉欢 <shenjiahuan@MacBook-Pro.local>
2018-07-19 14:35:33 +0800

    Code reformatted 4.0

---
+++ Draw.py
@@ -0,0 +1,73 @@
+from tkinter import *
+
+
+class Draw(object):
+    def __init__(self, game, row, col):
+        self.row = row
+        self.col = col
+        self.game = game
+        self.root = Tk()
+        self.root.title("连连看")
+        self.w = Canvas(self.root, width=game.width, height=game.height, background="white")
+        self.w.pack()
+        self.w.bind_all("<Button-1>", game.call_back)
+
+    def draw(self):
+        for row in range(self.row):
+            for col in range(self.col):
+                x0, y0, x1, y1 = self.game.get_pos(col, row)
+                color = self.game.get_color(col, row)
+                self.w.create_rectangle(x0, y0, x1, y1, fill=color, outline=color)
+
+    def draw_line(self, used):
+        for i in range(len(used) - 1):
+            begin_row, begin_col = used[i]
+            end_row, end_col = used[i + 1]
+            print((begin_row, begin_col), (end_row, end_col))
+            x0, y0, x1, y1 = self.game.get_pos(begin_col, begin_row)
+            x2, y2, x3, y3 = self.game.get_pos(end_col, end_row)
+            print((x0 + x1) / 2, (y0 + y1) / 2, (x2 + x3) / 2, (y2 + y3) / 2)
+            self.w.create_line((x0 + x1) / 2, (y0 + y1) / 2, (x2 + x3) / 2, (y2 + y3) / 2, fill="red", width=2)
+            self.w.after(300, self.erase_line, used)
+
+    def erase_line(self, used):
+        for i in range(len(used) - 1):
+            begin_row, begin_col = used[i]
+            end_row, end_col = used[i + 1]
+            print((begin_row, begin_col), (end_row, end_col))
+            x0, y0, x1, y1 = self.game.get_pos(begin_col, begin_row)
+            x2, y2, x3, y3 = self.game.get_pos(end_col, end_row)
+            print((x0 + x1) / 2, (y0 + y1) / 2, (x2 + x3) / 2, (y2 + y3) / 2)
+            self.w.create_line((x0 + x1) / 2, (y0 + y1) / 2, (x2 + x3) / 2, (y2 + y3) / 2, fill="white", width=2)
+
+    def highlight(self, point):
+        row, col = point
+        if self.game.highlighted is not None:
+            highlighted_row, highlighted_col = self.game.highlighted
+            x0, y0, x1, y1 = self.game.get_pos(highlighted_col, highlighted_row)
+            highlighted_color = self.game.get_color(highlighted_col, highlighted_row)
+            self.w.create_rectangle(x0, y0, x1, y1, fill=highlighted_color, outline=highlighted_color)
+        x2, y2, x3, y3 = self.game.get_pos(col, row)
+        color = self.game.get_color(col, row)
+        self.w.create_rectangle(x2, y2, x3, y3, fill=color, outline="#FFFFFF", dash=(10, 10))
+
+    def erase(self, original_point, target_point):
+        original_row, original_col = original_point
+        target_row, target_col = target_point
+        print(type(self.w))
+        self.game.matrix[original_row][original_col] = 0
+        self.game.matrix[target_row][target_col] = 0
+        x0, y0, x1, y1 = self.game.get_pos(original_col, original_row)
+        original_color = self.game.get_color(original_col, original_row)
+        self.w.create_rectangle(x0, y0, x1, y1, fill=original_color, outline=original_color)
+        x2, y2, x3, y3 = self.game.get_pos(target_col, target_row)
+        target_color = self.game.get_color(target_col, target_row)
+        self.w.create_rectangle(x2, y2, x3, y3, fill=target_color, outline=target_color)
+
+    def congratulations(self):
+        self.w.delete("all")
+        label = Label(self.root, text="恭喜你，通关啦！")
+        label.pack()
+
+    def play(self):
+        mainloop()
---
+++ Game.py
@@ -0,0 +1,156 @@
+import random
+import queue
+from Draw import *
+
+
+class Game(object):
+    size = 30
+    border = size // 8
+    offset = size
+    pos = (0, 0)
+    d = [[-1, 0], [0, -1], [1, 0], [0, 1]]
+    colors = ["#FFFFFF", "#000000", "#C0C0C0", "#FF0000", "#9C661F", "#B03060", "#FF00FF", "#FF9912",
+              "#5E2612", "#0000FF", "#1E90FF", "#00C78C", "#7CFC00", ]
+
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+        self.color_num = len(Game.colors) - 1
+        self.matrix = [[0 for _ in range(col)] for _ in range(row)]
+        self.chosen = None
+        self.highlighted = None
+        self.q = queue.Queue()
+        self.generate()
+        self.height, self.width = self.get_size()
+        self.drawer = Draw(self, row, col)
+        self.drawer.draw()
+
+    def play(self):
+        self.drawer.play()
+
+    def is_full(self):
+        for row in range(self.row):
+            for col in range(self.col):
+                if self.matrix[row][col] == 0:
+                    return False
+        return True
+
+    def is_empty(self):
+        for row in range(self.row):
+            for col in range(self.col):
+                if self.matrix[row][col] != 0:
+                    return False
+        return True
+
+    def generate(self):
+        for color in range(1, self.color_num + 1):
+            count = 0
+            while count < int(self.row * self.col / self.color_num) and not self.is_full():
+                random_row = random.randint(0, self.row - 1)
+                random_col = random.randint(0, self.col - 1)
+                if self.matrix[random_row][random_col] == 0:
+                    self.matrix[random_row][random_col] = color
+                    count += 1
+
+    @staticmethod
+    def get_pos(x, y):
+        x0 = x * (Game.size + Game.border) + Game.border + Game.offset
+        y0 = y * (Game.size + Game.border) + Game.border + Game.offset
+        x1 = x0 + Game.size
+        y1 = y0 + Game.size
+        return x0, y0, x1, y1
+
+    def get_color(self, x, y):
+        return Game.colors[self.matrix[y][x]]
+
+    def get_size(self):
+        return self.row * (Game.size + Game.border) + Game.border + Game.offset * 2, \
+               self.col * (Game.size + Game.border) + Game.border + Game.offset * 2
+
+    def point_outside_grid(self, event, point):
+        row, col = point
+        if event.x < (Game.size + Game.border) * col + Game.border \
+                or event.y < (Game.size + Game.border) * row + Game.border:
+            return True
+        if col < 0 or row < 0 or col >= self.col or row >= self.row:
+            return True
+
+    def call_back(self, event):
+        col = (event.x - Game.offset) // (Game.size + Game.border)
+        row = (event.y - Game.offset) // (Game.size + Game.border)
+        if self.point_outside_grid(event, [row, col]):
+            return
+        color = self.get_color(col, row)
+        if color == "#FFFFFF":
+            return
+        self.drawer.highlight([row, col])
+        self.highlighted = (row, col)
+        if self.chosen is None or self.chosen == (row, col):
+            self.chosen = (row, col)
+        else:
+            original_row, original_col = self.chosen
+            self.chosen = (row, col)
+            original_color = self.get_color(original_col, original_row)
+            color = self.get_color(col, row)
+            if original_color != color:
+                return
+            self.q.queue.clear()
+            self.q.put((original_row, original_col, [(original_row, original_col)], None, 0))
+            self.bfs((original_row, original_col), (row, col), color)
+
+    def step_result(self, q_elem, target_point, target_color, d):
+        row, col, used, last_pos, turns = q_elem
+        target_row, target_col = target_point
+        d_row, d_col = d
+        if (row + d_row, col + d_col) in used:
+            return 0
+        if row + d_row < -1 or row + d_row > self.row or col + d_col < -1 or col + d_col > self.col:
+            return 0
+        if 0 <= row + d_row <= self.row - 1 and 0 <= col + d_col <= self.col - 1:
+            color = self.get_color(col + d_col, row + d_row)
+            if color != "#FFFFFF" and color != target_color:
+                return 0
+            if color != "#FFFFFF" and (row + d_row, col + d_col) != (target_row, target_col):
+                return 0
+            if color == "#FFFFFF":
+                return 1
+            elif color == target_color and row + d_row == target_row and col + d_col == target_col:
+                if (d_row, d_col) != last_pos and turns > 2:
+                    return 0
+                else:
+                    return 2
+        else:
+            return 1
+
+    def bfs(self, original_point, target_point, target_color):
+        original_row, original_col = original_point
+        target_row, target_col = target_point
+        found = False
+        while not self.q.empty() and not found:
+            row, col, used, last_pos, turns = q_elem = self.q.get()
+            if turns > 3:
+                continue
+            for d_row, d_col in Game.d:
+                result = self.step_result(q_elem, target_point, target_color, (d_row, d_col))
+                if result == 0:
+                    continue
+                if result == 1:
+                    used.append((row + d_row, col + d_col))
+                    last_pos_bak = last_pos
+                    if (d_row, d_col) != last_pos:
+                        turns += 1
+                    last_pos = (d_row, d_col)
+                    self.q.put((row + d_row, col + d_col, used[:], last_pos, turns))
+                    used.pop()
+                    if last_pos_bak != last_pos:
+                        last_pos = last_pos_bak
+                        turns -= 1
+                elif result == 2:
+                    used.append((target_row, target_col))
+                    self.drawer.erase((original_row, original_col), (target_row, target_col))
+                    self.drawer.draw_line(used)
+                    if self.is_empty():
+                        self.drawer.congratulations()
+                    self.chosen = None
+                    found = True
+                    break
